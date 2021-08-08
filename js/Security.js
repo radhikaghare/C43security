2b2f3f7c7cafc68485e61c08dea0921a4939e723
radhikaghare <58420969+radhikaghare@users.noreply.github.com>
2021-08-08 17:19:46 +0530

    Update Security.js

--- js/Security.js
+++ js/Security.js
@@ -6,10 +6,7 @@ class Security {
         this.access1.position(100,90);
         this.access1.style('background', 'white');  
 
-        this.button1 = createButton('Check');
-        this.button1.position(100,120);
-        this.button1.style('background', 'lightgrey');    
-
+       
         this.access2 = createInput("Code1")
         this.access2.position(700,190);
         this.access2.style('background', 'white');  
@@ -29,12 +26,7 @@ class Security {
 
     display(){
 
-        this.button1.mousePressed(() => {
-            if(system.authenticate(accessCode1,this.access1.value())){
-                this.button1.hide();
-                this.access1.hide();
-                score++;
-            }
+      
         });
 
         this.button2.mousePressed(() => {
